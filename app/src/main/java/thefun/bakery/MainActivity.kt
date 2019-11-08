a50b11bcdf264b2087ea33bf1eef69bf35ba559e
kelly <kelly.0x00@gmail.com>
2019-11-08 09:53:12 +0900

    프로젝트 생성 및 메인화면 추가

---
+++ app/src/main/java/thefun/bakery/MainActivity.kt
@@ -0,0 +1,17 @@
+package thefun.bakery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.main_title)?.let {
+            it.text = "Kelly's Home"    //TODO remove
+        }
+    }
+}
