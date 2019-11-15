c38a38ea6718383d3cc3b08d772a276c42d9a17f
kelly <kelly.0x00@gmail.com>
2019-11-15 16:52:42 +0900

    메인 레이아웃 수정, 하단 메뉴에 셀렉터 넣음.

--- app/src/main/java/thefun/bakery/MainActivity.kt
+++ app/src/main/java/thefun/bakery/MainActivity.kt
@@ -2,6 +2,9 @@ package thefun.bakery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +16,16 @@ class MainActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.main_title)?.let {
             it.text = "Kelly's Home"    //TODO remove
         }
+
+        findViewById<ImageView>(R.id.create_main_content).setOnClickListener {
+
+        }
+//        val testEditor = findViewById<EditText>(R.id.test_editor)
+//        testEditor.setText("")
+//        findViewById<ImageView>(R.id.main_content_bg)?.let {
+//            it.setOnClickListener {
+//                testEditor.performClick()
+//            }
+//        }
     }
 }
